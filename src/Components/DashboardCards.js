de3ad3d09fb4f737800a090eeea56b5ef1a9960a
adil2190 <madil219076@gmail.com>
2022-01-01 04:53:42 +0500

    changes

--- src/Components/DashboardCards.js
+++ src/Components/DashboardCards.js
@@ -1,7 +1,29 @@
-import React from "react";
+import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import demo from "../Assets/form_background.jpg";
 
-function DashboardCards(props) {
-  return <div></div>;
+export default function DashboardCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" height="140" image={demo} alt="green iguana" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
-
-export default DashboardCards;
--- src/Pages/DashboardDefault.js
+++ src/Pages/DashboardDefault.js
@@ -1,7 +1,13 @@
 import React from "react";
+import DashboardCard from "../Components/DashboardCards";
 
 function DashboardDefault(props) {
-  return <div>DASHBOARD</div>;
+  return (
+    <div>
+      <h1 className="dashboard-heading">DASHBOARD</h1>
+      <DashboardCard />
+    </div>
+  );
 }
 
 export default DashboardDefault;
